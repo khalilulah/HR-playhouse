439a5690380b032ad8ff00822c926be4b412b90d
khalilulah <alausakhalil@gmail.com>
2025-12-25 15:46:09 +0100

    testing the hero

--- src/components/ResourceHero.jsx
+++ src/components/ResourceHero.jsx
@@ -5,6 +5,22 @@ import Freepik2 from "../assets/images/Freepik2.webp";
 function ResourceHero() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [hoveredImage, setHoveredImage] = useState(null);
+  const [isLargeHeight, setIsLargeHeight] = useState(true);
+
+  useEffect(() => {
+    const checkHeight = () => {
+      setIsLargeHeight(window.innerHeight > 570);
+    };
+
+    // Check on mount
+    checkHeight();
+
+    // Check on resize
+    window.addEventListener("resize", checkHeight);
+
+    // Cleanup
+    return () => window.removeEventListener("resize", checkHeight);
+  }, []);
 
   useEffect(() => {
     // Trigger animations after component mounts
@@ -15,7 +31,13 @@ function ResourceHero() {
   }, []);
 
   return (
-    <div className="mx-auto max-w-400 flex justify-center flex-col xl:flex-row gap-8 lg:gap-12 xl:gap-16 items-center px-4 sm:px-6 lg:px-8 perspective-[1500px] sm:h-auto my-12 md:my-16 lg:my-20 py-3 overflow-hidden">
+    <div
+      className={`mx-auto max-w-400 flex justify-center flex-col xl:flex-row gap-8 lg:gap-12 xl:gap-16 items-center px-4 sm:px-6 lg:px-8 perspective-[1500px] overflow-hidden${
+        isLargeHeight
+          ? "h-[calc(100vh-3.8rem)] sm:h-[calc(100vh-4rem)] md:h-[calc(100vh-5rem)] pb-[2rem] md:pb-[2.5rem]"
+          : "sm:h-auto my-12 md:my-16 lg:my-20 py-3"
+      }`}
+    >
       {/* Text Section */}
       <div className="w-full xl:w-1/2 text-center xl:text-left">
         <p
